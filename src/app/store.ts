4ff40e625e42dfee794d596ee4a76fd8883e7534
Vasily Mitronov <vasmitr95@gmail.com>
2022-08-18 14:55:05 +0000

    Clean up

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,9 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import weatherReducer from '../features/weather/weatherSlice';
 import searchReducer from '../features/search/searchSlice'
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     weather: weatherReducer,
     search: searchReducer,
   },
